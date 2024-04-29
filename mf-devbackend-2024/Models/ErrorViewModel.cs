9f700e7907d9bf97f582357b1b834cb88fe8bfb1
anamantea <129327473+anamantea@users.noreply.github.com>
2024-04-29 19:10:41 -0300

    first commit

---
+++ mf-devbackend-2024/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace mf_devbackend_2024.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
